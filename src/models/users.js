2e9661dd3d9711b931b4e281aea9b1f74242fd65
totireynal <sofiareynalm@gmail.com>
2023-03-15 19:16:25 -0300

    relaciones 1:1

--- src/models/users.js
+++ src/models/users.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Users.hasOne(models.File),
+      Users.hasOne(models.UserPassword)
     }
   }
   Users.init({
