bc518cd1aadcb83b3c554e9481a11c3a600d9a41
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-15 14:05:46 -0500

    Cambio de puerto en el index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app');
 // const { Sequelize } = require('./src/models/index');
 require('dotenv').config();
-const { PORT } = process.env;
+const PORT = 3001;
 
 
 // ({ alter: true }).then(() => 
