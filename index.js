5b62aa8f653cdf10a8c854ec8981b711cc6286b2
totireynal <sofiareynalm@gmail.com>
2023-03-15 15:29:55 -0300

    creacion db con cli

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const server = require('./src/app');
-const { database } = require('./src/db');
-require('dotenv').config();
-const {PORT} = process.env;
+const PORT = 3001
 
 
-database.sync({alter: true }).then(() => 
-console.log('DB connected'),
 server.listen(PORT, () => {
 console.log(`Server connected on ${PORT}` )
-}));
+});
