c26b37f2e0825de0bb2d28a69b05389668b7a967
totireynal <sofiareynalm@gmail.com>
2023-03-15 17:09:31 -0300

    arreglo index

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const server = require('./src/app');
-<<<<<<< HEAD
+
 const PORT = 3001
 
-
-=======
-require('dotenv').config();
-const PORT = 3001;
-
-
-// sequelize.sync({alter: true }).then(() => 
-// console.log('DB connected'),
->>>>>>> bb1ed37405f4ae2dc74d9200352cef780c323d9b
 server.listen(PORT, () => {
 console.log(`Server connected on ${PORT}` )
 });
