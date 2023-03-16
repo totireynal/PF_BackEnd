181e7588eff55578d68526ef40e7e013f8f8fdbf
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-16 17:52:22 -0500

    Ruta get Users y nombre

---
+++ src/handlers/getRoutesHandlers/getUsersHandler.js
@@ -0,0 +1,16 @@
+const getUsersController = require('../../controllers/getRoutesController/getUsersController');
+const getUserNameController = require('../../controllers/getRoutesController/getUserNameController');
+
+const getUsersHandler = async(req, res) => {
+    const { name } = req.query;
+
+    try {
+        const resultsUsers = name ? await getUserNameController(name) : await getUsersController();
+        console.log(resultsUsers);
+        return res.status(201).json(resultsUsers);
+    } catch (error) {
+        return res.status(404).json({ error: error.message })
+    }
+};
+
+module.exports = getUsersHandler;
