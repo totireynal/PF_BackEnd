60b1e56d1fff075a2f0c864093bf5b43483ccb24
totireynal <sofiareynalm@gmail.com>
2023-03-16 19:57:52 -0300

    nuevas rutas

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,9 +3,11 @@ const postUsersHandler = require('../handlers/postUsersHandler');
 const validatePostUsers = require('../middlewares/validatePostUsers');
 const postFilesHandler = require('../handlers/postFilesHandler');
 const validatePostFiles= require('../middlewares/validatePostFiles');
+const getAllUsers = require('../handlers/getAllUsers');
 
 
 usersRouter.post('/', validatePostUsers, postUsersHandler);
 usersRouter.post('/files', validatePostFiles, postFilesHandler);
+usersRouter.get('/', getAllUsers)
 
 module.exports = usersRouter;
