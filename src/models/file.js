1395529851c580ab129d1166aa36c8c9486354dd
totireynal <sofiareynalm@gmail.com>
2023-03-16 00:46:19 -0300

    solucionado error relacion

--- src/models/file.js
+++ src/models/file.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      File.belongsTo(models.User)
+      File.belongsTo(models.Users)
     }
   }
   File.init(
