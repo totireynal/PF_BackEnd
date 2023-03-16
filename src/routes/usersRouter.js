b4fca1a4c8ebfa456095405454941fe76a608b31
totireynal <sofiareynalm@gmail.com>
2023-03-16 00:59:45 -0300

    ruta postUsers

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,4 +1,7 @@
 const usersRouter = require('express').Router();
+const postUsersHandler = require('../handlers/postUsersHandlers');
 
+
+usersRouter.post('/', postUsersHandler)
 
 module.exports = usersRouter;
