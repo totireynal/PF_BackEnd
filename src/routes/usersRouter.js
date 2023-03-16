81b138d5538bcf12fc2cd5e5a81ebc6b72ff1840
totireynal <sofiareynalm@gmail.com>
2023-03-16 16:30:21 -0300

    nueva ruta post

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,11 @@
 const usersRouter = require('express').Router();
-const postUsersHandler = require('../handlers/postUsersHandlers');
+const postUsersHandler = require('../handlers/postUsersHandler');
 const validatePostUsers = require('../middlewares/validatePostUsers');
+const postFilesHandler = require('../handlers/postFilesHandler');
+const validatePostFiles= require('../middlewares/validatePostFiles');
 
 
-usersRouter.post('/', validatePostUsers, postUsersHandler)
+usersRouter.post('/', validatePostUsers, postUsersHandler);
+usersRouter.post('/files', validatePostFiles, postFilesHandler);
 
 module.exports = usersRouter;
