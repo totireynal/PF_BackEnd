02811c4bb7c14a665bc4284ccd6a7a6c1d1b912b
totireynal <sofiareynalm@gmail.com>
2023-03-16 12:07:13 -0300

    relacion arreglada

--- src/models/users.js
+++ src/models/users.js
@@ -11,7 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Users.hasOne(models.File),
-      Users.hasOne(models.UserPassword)
+      Users.hasOne(models.UserPassword),
+      Users.belongsTo(models.Company)
     }
   }
   Users.init({
