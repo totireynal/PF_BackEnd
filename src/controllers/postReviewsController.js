512550fe431947aa41bc3b653468153b0c29a0a8
totireynal <sofiareynalm@gmail.com>
2023-03-17 00:27:56 -0300

    controllers review

---
+++ src/controllers/postReviewsController.js
@@ -0,0 +1,14 @@
+const Review = require('../models/').Review;
+
+const postReviewsController = async (scores, comment, CompanyId) => {
+    let newReview = await Review.create({
+        scores,
+        comment
+    })
+
+    await newReview.setCompany(CompanyId)
+
+    return `The review has been created correctly.`;
+}
+
+module.exports = postReviewsController;
