2803f851b3c08638131fab5fc4b048ee4fdebd45
totireynal <sofiareynalm@gmail.com>
2023-03-17 16:49:33 -0300

    put tanto para users como files

--- src/controllers/usersControllers/putUsersController.js
+++ src/controllers/usersControllers/putUsersController.js
@@ -1,19 +1,31 @@
+const { sequelize } = require('../../models');
 const Users = require('../../models').Users;
+const File = require('../../models').File;
 
-const putUsersController = async (id, name, lastName, email, birthDate, address, image, dni, tel, role) => {
+const putUsersController = async (id, name, lastName, email, birthDate, address, image, dni, tel, role, dateOfAdmission, position, area, cuil, cbu) => {
+      
+      const findUserById = await Users.findOne({
+           where: 
+               { id:id } 
+      });
 
-   const findUserById = await Users.findOne({
-        where: 
-            { id:id } 
-   })
+      if (!findUserById) throw new Error(`The user with id: ${id} doesn't exist.`)
 
-   if (!findUserById) throw new Error(`The user with id: ${id} doesn't exist.`)
+      
+      await Users.update({
+       name, lastName, email, birthDate, address, image, dni, tel, role},
+       {where: { id:id }
+      });
 
-   await Users.update({
-    name, lastName, email, birthDate, address, image, dni, tel, role},
-    {where: { id:id }})
 
-   return `The user with id: ${id} has been updated correctly.`
+      await File.update({
+        dateOfAdmission, position, area, cuil, cbu},
+        {where: {UserId: id}
+      });
+
+
+      return `The user with id: ${id} has been updated correctly.`
+
 
 };
 
