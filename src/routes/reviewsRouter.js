4d1ae0d7f4c21be6e933429479372f83b958bc0d
juanavila <juan.avila@live.com.ar>
2023-03-17 19:53:25 -0300

    arreglo de moco

--- src/routes/reviewsRouter.js
+++ src/routes/reviewsRouter.js
@@ -10,3 +10,11 @@ reviewsRouter.get('/', getReviewsHandler);
 
 module.exports = reviewsRouter;
 
+const informationRouter = require('express').Router();
+const getOurInfoHandler = require('../handlers/informationHandlers/getOurInfoHandler');
+
+
+informationRouter.get('/', getOurInfoHandler);
+
+
+module.exports = informationRouter;
