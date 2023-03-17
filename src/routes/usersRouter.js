484a239d1fb143e18e24429e28bec524715e8f1a
totireynal <sofiareynalm@gmail.com>
2023-03-17 15:46:31 -0300

    arreglo rutas

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,7 @@
 const usersRouter = require('express').Router();
 const validatePostUsers = require('../middlewares/validatePostUsers');
-const validatePostFiles= require('../middlewares/validatePostUsersbyCuilAndCbu');
 const putUserHandler = require('../handlers/usersHandlers/putUsersHandler');
 const validatePutUsers = require('../middlewares/validatePutUsers');
-const postFilesHandler = require('../handlers/filesHandlers/postFilesHandler');
 const postUsersHandler = require('../handlers/usersHandlers/postUsersHandler');
 const getUsersHandler = require('../handlers/usersHandlers/getUsersHandler');
 const softDeleteUsers = require('../middlewares/validateSoftDeleteUsers');
@@ -14,7 +12,6 @@ const validatePostUsersByCuilAndCbu = require('../middlewares/validatePostUsersb
 
 usersRouter.get('/', getUsersHandler);
 usersRouter.post('/', validatePostUsers, validatePostUsersByCuilAndCbu, postUsersHandler);
-usersRouter.post('/files', validatePostFiles, postFilesHandler);
 usersRouter.put('/:id',validatePutUsers, putUserHandler);
 usersRouter.delete('/:id', softDeleteUsers, softDeleteHandler);
 
