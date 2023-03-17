254fbeaaf8951df47a8ac1594469cd084f178ce6
totireynal <sofiareynalm@gmail.com>
2023-03-17 00:28:28 -0300

    handlers reviews

---
+++ src/handlers/postReviewsHandler.js
@@ -0,0 +1,17 @@
+const postReviewsController = require('../controllers/postReviewsController');
+
+
+const postReviewsHandler = async (req, res) => {
+
+    const { scores, comment, CompanyId } = req.body;
+
+    try {
+        const newReview = await postReviewsController(scores, comment, CompanyId)
+        res.status(200).json(newReview);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+module.exports = postReviewsHandler;
