5eab70041a6afa1850576a972d2eb836de8ebacd
totireynal <sofiareynalm@gmail.com>
2023-03-16 23:14:05 -0300

    ruta postCompaniespor corregir

--- src/handlers/postCompaniesHandler.js
+++ src/handlers/postCompaniesHandler.js
@@ -1,6 +1,18 @@
-const getCompaniesHandler = async (req, res) => {
+const postCompaniesController = require('../controllers/postCompaniesController');
+
+const postCompaniesHandler = async (req, res) => {
+    const { name, cuit, industry, location, numberEmployees, tel, email} = req.body
     
+    try {
+    
+        const newCompany = await postCompaniesController(name, cuit, industry, location, numberEmployees, tel, email)
+        res.status(200).json(newCompany);
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    
+}
 }
 
 
-module.exports = getCompaniesHandler;
+module.exports = postCompaniesHandler;
