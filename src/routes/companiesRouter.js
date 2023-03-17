3b481643482aa0515d511e8dc4919c778c797678
totireynal <sofiareynalm@gmail.com>
2023-03-16 23:48:00 -0300

    agregado el middleware

--- src/routes/companiesRouter.js
+++ src/routes/companiesRouter.js
@@ -1,6 +1,7 @@
 const companiesRouter = require('express').Router();
 const postCompaniesHandler = require('../handlers/postCompaniesHandler');
+const validatePostCompanies = require('../middlewares/validatePostCompanies');
 
-companiesRouter.post('/register', postCompaniesHandler)
+companiesRouter.post('/register', validatePostCompanies, postCompaniesHandler)
 
 module.exports = companiesRouter;
