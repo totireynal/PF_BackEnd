aa1cbef4e6d9d900f7070c5f63506f74884d57f4
nottotem <totemstu@gmail.com>
2023-03-17 20:30:19 -0300

    middelware corregido

--- src/middlewares/validatePostUsers.js
+++ src/middlewares/validatePostUsers.js
@@ -33,29 +33,6 @@ const validatePostUsers = (req, res, next) => {
   if (!cuil) return res.status(400).json({ error: "Missing cuil" });
   if (!cbu) return res.status(400).json({ error: "Missing cbu" });
 
-  const {
-    name,
-    lastName,
-    email,
-    birthDate,
-    address,
-    image,
-    dni,
-    tel,
-    role,
-    CompanyId,
-  } = req.body;
-  if (!name) return res.status(400).json({ error: "Missing name" });
-  if (!lastName) return res.status(400).json({ error: "Missing lastname" });
-  if (!email) return res.status(400).json({ error: "Missing email" });
-  if (!birthDate) return res.status(400).json({ error: "Missing birthDate" });
-  if (!address) return res.status(400).json({ error: "Missing address" });
-  if (!image) return res.status(400).json({ error: "Missing image" });
-  if (!dni) return res.status(400).json({ error: "Missing dni" });
-  if (!tel) return res.status(400).json({ error: "Missing tel" });
-  if (!role) return res.status(400).json({ error: "Missing role" });
-  // if (!CompanyId) return res.status(400).json({error: 'Missing CompanyId'});
-
   next();
 };
 
