27c7ed10aeff038596b33362768f442ab3b3ce69
totireynal <sofiareynalm@gmail.com>
2023-03-17 15:29:21 -0300

    anexadas ruta post users y files

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,6 +1,6 @@
 const usersRouter = require('express').Router();
 const validatePostUsers = require('../middlewares/validatePostUsers');
-const validatePostFiles= require('../middlewares/validatePostFiles');
+const validatePostFiles= require('../middlewares/validatePostUsersbyCuilAndCbu');
 const putUserHandler = require('../handlers/usersHandlers/putUsersHandler');
 const validatePutUsers = require('../middlewares/validatePutUsers');
 const postFilesHandler = require('../handlers/filesHandlers/postFilesHandler');
@@ -8,11 +8,12 @@ const postUsersHandler = require('../handlers/usersHandlers/postUsersHandler');
 const getUsersHandler = require('../handlers/usersHandlers/getUsersHandler');
 const softDeleteUsers = require('../middlewares/validateSoftDeleteUsers');
 const softDeleteHandler = require('../handlers/usersHandlers/deleteUsersHandlers');
+const validatePostUsersByCuilAndCbu = require('../middlewares/validatePostUsersbyCuilAndCbu');
 
 
 
 usersRouter.get('/', getUsersHandler);
-usersRouter.post('/', validatePostUsers, postUsersHandler);
+usersRouter.post('/', validatePostUsers, validatePostUsersByCuilAndCbu, postUsersHandler);
 usersRouter.post('/files', validatePostFiles, postFilesHandler);
 usersRouter.put('/:id',validatePutUsers, putUserHandler);
 usersRouter.delete('/:id', softDeleteUsers, softDeleteHandler);
