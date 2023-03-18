264ef530b678a76f8e6e05f2b7791ae41debf855
nottotem <totemstu@gmail.com>
2023-03-18 17:45:56 -0300

    back al 100%

--- src/controllers/usersControllers/getUserNameController.js
+++ src/controllers/usersControllers/getUserNameController.js
@@ -1,26 +1,28 @@
-const { Op } = require('sequelize');
-const Users = require('../../models').Users;
-const File = require('../../models').File;
-const cleanInfoDb = require('../../utils/getUsersCleanDb');
+const { Op } = require("sequelize");
+const Users = require("../../models").Users;
+const File = require("../../models").File;
+const cleanInfoDb = require("../../utils/getUsersCleanDb");
 
-const getUserNameController = async(name) => {
-    const dataBaseNameRaw = await File.findAll({
-        include: {
-            model: Users,
-            attributes: ['name','lastName', 'image'],
-            where: {
-                name: {
-                    [Op.iLike] : `%${name}%`
-                }
-            },  
-        }
-       
-    });
-    const cleanInfo = cleanInfoDb(dataBaseNameRaw);
+const getUserNameController = async (name) => {
+  const dataBaseNameRaw = await File.findAll({
+    include: {
+      model: Users,
+      attributes: ["name", "lastName", "image", "role"],
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`,
+        },
+      },
+    },
+  });
+  const cleanInfo = cleanInfoDb(dataBaseNameRaw);
 
-    if(cleanInfo.length === 0) throw new Error(`The user with the name: '${name}' does not exist. Try with another please.`);
+  if (cleanInfo.length === 0)
+    throw new Error(
+      `The user with the name: '${name}' does not exist. Try with another please.`
+    );
 
-    return cleanInfo;
+  return cleanInfo;
 };
 
 module.exports = getUserNameController;
