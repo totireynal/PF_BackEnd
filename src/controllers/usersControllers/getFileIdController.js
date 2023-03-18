97c92abffef22c718d2abc7263dcabb1e5a2468d
nottotem <totemstu@gmail.com>
2023-03-18 16:24:21 -0300

    back actualiz

--- src/controllers/usersControllers/getFileIdController.js
+++ src/controllers/usersControllers/getFileIdController.js
@@ -1,22 +1,19 @@
-const File = require('../../models').File;
-const Users = require('../../models').Users;
+const File = require("../../models").File;
+const Users = require("../../models").Users;
 
-const getFileIdController = async(id) => {
-  
-        const fileIdDb = await File.findOne({
-            where: {
-                UserId: id
-            },
-            include: {
-                model: Users,
-               
-            }
-        });
+const getFileIdController = async (id) => {
+  const fileIdDb = await File.findOne({
+    where: {
+      UserId: id,
+    },
+    include: {
+      model: Users,
+    },
+  });
 
-        if(!fileIdDb) throw new Error(`The file with id: '${id}' not exist` )
+  if (!fileIdDb) throw new Error(`The file with id: '${id}' not exist`);
 
-        return fileIdDb;
-
+  return fileIdDb;
 };
 
 module.exports = getFileIdController;
