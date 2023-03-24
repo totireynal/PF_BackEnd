860e6b6db903e45915c92c80751bf07c8da62764
juanavila <juan.avila@live.com.ar>
2023-03-23 22:46:18 -0300

    post users ok

--- src/handlers/usersHandlers/postUsersHandler.js
+++ src/handlers/usersHandlers/postUsersHandler.js
@@ -4,10 +4,10 @@ const postUsersController = require('../../controllers/usersControllers/postCont
 
 const postUsersHandler = async (req, res) => {
 
-    const { name, lastName, email, birthDate, address, image, dni, tel, role, CompanyId, dateOfAdmission, position, area, cuil, cbu} = req.body;
+    const { name, lastName, email, birthDate, address, image, dni, tel , role,CompanyId, PositionId, AreaId,  dateOfAdmission, cuil, cbu} = req.body;
 
     try {
-        const newUser = await postUsersController(name, lastName, email, birthDate, address, image, dni, tel, role, CompanyId, dateOfAdmission, position, area, cuil, cbu)
+        const newUser = await postUsersController(name, lastName, email, birthDate, address,CompanyId, image, dni, tel, role,  PositionId , AreaId , dateOfAdmission, cuil, cbu)
         res.status(200).json(newUser);
     } catch (error) {
         res.status(400).json({error: error.message})
