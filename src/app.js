ca1e4378dae17517ae6e403b8b794286ff6c9a7d
nottotem <totemstu@gmail.com>
2023-03-24 13:10:40 -0300

    form notific

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const usersRouter = require('./routes/usersRouter');
-const informationRouter = require('./routes/informationRouter');
-const companiesRouter = require('./routes/companiesRouter');
-const reviewsRouter = require('./routes/reviewsRouter');
-const positionsRouter = require('./routes/positionsRouter');
-const areasRouter = require('./routes/areasRouter');
-const rolesRouter = require('./routes/rolesRouter');
-
-
+const usersRouter = require("./routes/usersRouter");
+const informationRouter = require("./routes/informationRouter");
+const companiesRouter = require("./routes/companiesRouter");
+const reviewsRouter = require("./routes/reviewsRouter");
+const positionsRouter = require("./routes/positionsRouter");
+const areasRouter = require("./routes/areasRouter");
+const rolesRouter = require("./routes/rolesRouter");
+const sendgridRouter = require("./routes/sendgridRouter");
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 
-server.use('/users', usersRouter);
-server.use('/info', informationRouter);
-server.use('/companies', companiesRouter);
-server.use('/reviews', reviewsRouter);
-server.use('/positions', positionsRouter);
-server.use('/areas', areasRouter);
-server.use('/roles', rolesRouter)
+server.use("/users", usersRouter);
+server.use("/info", informationRouter);
+server.use("/companies", companiesRouter);
+server.use("/reviews", reviewsRouter);
+server.use("/positions", positionsRouter);
+server.use("/areas", areasRouter);
+server.use("/roles", rolesRouter);
+server.use("/notifications", sendgridRouter);
 
 module.exports = server;
--- src/controllers/sendGridController/sendGridController.js
+++ src/controllers/sendGridController/sendGridController.js
@@ -3,6 +3,5 @@ const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// messageMail.setApiKey('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');
 
 module.exports = sgMail;
