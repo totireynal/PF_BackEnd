a476e418ef0e865b89f8956f0ce1cf23dae79b65
Carlad0 <martinezcarlosar@gmail.com>
2023-03-25 23:48:05 -0300

    Auth

--- src/middlewares/authMiddlewares/checkAutorization.js
+++ src/middlewares/authMiddlewares/checkAutorization.js
@@ -8,8 +8,8 @@ const checkAuthorization = async (req, res) => {
     const response = await axios('https://dev-a5lp6h1utxb70h27.us.auth0.com/userinfo', {
         headers: {
             authorization: `Bearer ${req.auth.token}`
-            }
-        })
+        }
+    })
 
     let email = response.data.email;
     const check = await Users.findOne({
@@ -20,28 +20,24 @@ const checkAuthorization = async (req, res) => {
         
     // console.log(check.dataValues.CompanyId)
     if (check===null){ 
-    res.status(403).send('Not authorized')
+        res.status(403).send('Not authorized')
     } else {
 
+        let name = check.dataValues.name;
+        let lastName = check.dataValues.lastName
+        let CompanyId = check.dataValues.CompanyId;
+        let role = check.dataValues.role;
 
-    let name = check.dataValues.name;
-    let lastName = check.dataValues.lastName
-    let CompanyId = check.dataValues.CompanyId;
-    let role = check.dataValues.role;
+        const payload = {
+            name: name,
+            lastName: lastName,
+            CompanyId: CompanyId,
+            role: role
+        }
 
-    const payload = {
-        name: name,
-        lastName: lastName,
-        CompanyId: CompanyId,
-        role: role
+        const newToken = jwt.sign(payload, '123456')
+        res.status(200).json({token : newToken})
     }
-
-    const newToken = jwt.sign(payload, '123456')
-
-
-    res.status(200).json({token : newToken})
-
-}
 }
 
 module.exports = checkAuthorization
