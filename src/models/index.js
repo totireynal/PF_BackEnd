3faed33a2575b1fbff67f4c053cbf65f0f5f6034
totireynal <sofiareynalm@gmail.com>
2023-03-28 20:19:46 -0300

    deploy

--- src/models/index.js
+++ src/models/index.js
@@ -8,12 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '../../config/config.js')[env];
 const db = {};
 
-
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env.DB_DEPLOY);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(process.env.DB_DEPLOY);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
