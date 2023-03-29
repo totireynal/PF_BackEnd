59bef723a8a7b032636e8b8e0d96b6722fa45ee5
totireynal <sofiareynalm@gmail.com>
2023-03-28 23:17:39 -0300

    deploy

--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,19 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '../../config/config.js')[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '../../config/config.js')[env];
+require('dotenv').config()
 const db = {};
 console.log(process.env.DB_DEPLOY)
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env.DB_DEPLOY);
-} else {
-  sequelize = new Sequelize(process.env.DB_DEPLOY);
-}
+
+let sequelize = new Sequelize(process.env.DB_DEPLOY)
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env.DB_DEPLOY);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_DEPLOY);
+// }
 
 fs
   .readdirSync(__dirname)
@@ -38,6 +41,7 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 
 
 module.exports = db;
