efd2fca10df2bf3da24a2ae8596006dccbf9ae22
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-29 10:08:23 -0500

    Cambio para trabajar

--- src/models/index.js
+++ src/models/index.js
@@ -2,28 +2,18 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-// const process = require('process');
+const process = require('process');
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '../../config/config.js')[env];
-require('dotenv').config();
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '../../config/config.js')[env];
 const db = {};
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-let sequelize = new Sequelize(process.env.DB_DEPLOY, {
-  logging: false,
-  native: false
-});
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
