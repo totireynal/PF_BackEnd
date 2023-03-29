bcce83c2bd42aeb9fbe52efeeaae1ead225888f4
totireynal <sofiareynalm@gmail.com>
2023-03-29 09:47:34 -0300

    intento deploy railway

--- src/config/config.js
+++ src/config/config.js
@@ -6,9 +6,9 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: 'process.env.DB_HOST' || "127.0.0.1",
     dialect: "postgres",
-    deploy: process.env.DB_DEPLOY
+
   },
   test: {
     username: process.env.DB_USER,
@@ -21,8 +21,8 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: 'process.env.DB_HOST' || "127.0.0.1",
     dialect: "postgres",
-    deploy: process.env.DB_DEPLOY
+
   },
 };
--- src/models/index.js
+++ src/models/index.js
@@ -6,10 +6,9 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '../../config/config.js')[env];
-require('dotenv').config()
 const db = {};
-console.log(process.env.DB_DEPLOY)
 
+console.log(config)
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
