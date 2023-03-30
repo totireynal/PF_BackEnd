7835ce59a8c9a18635625a158a819fd79a5e2746
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-30 10:34:34 -0500

    El pepe

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,21 +3,25 @@ const validatePostUsers = require('../middlewares/userMiddlewares/validatePostUs
 const validatePutUsers = require('../middlewares/userMiddlewares/validatePutUsers');
 const softDeleteUsers = require('../middlewares/userMiddlewares/validateSoftDeleteUsers');
 const softDeleteHandler = require('../handlers/usersHandlers/deleteUsersHandlers');
+const getUsersHandler = require('../handlers/usersHandlers/getUsersHandler');
 const putUserHandler = require('../handlers/usersHandlers/putUsersHandler');
 const postUsersHandler = require('../handlers/usersHandlers/postUsersHandler');
 const validatePostUsersByCuilAndCbu = require('../middlewares/userMiddlewares/validatePostUsersbyCuilAndCbu');
 const getDetailHandler = require('../handlers/usersHandlers/getDetailHandler');
 
 
+
 // usersRouter.use(verifyJWT)
-usersRouter.get('/:CompanyId', getUsersHandler);
-usersRouter.get('/:CompanyId/:id', getDetailHandler);
+
+usersRouter.get('/:CompanyId', getUsersHandler);  // trae todos y agrega queries para filtrar
+usersRouter.get('/:CompanyId/:id', getDetailHandler); // trae detail del empleado
+// usersRouter.post('/', validatePostUsers, postUsersHandler);
 usersRouter.post('/', validatePostUsers, validatePostUsersByCuilAndCbu, postUsersHandler);
 usersRouter.put('/:id',validatePutUsers, putUserHandler);
 usersRouter.delete('/:id', softDeleteUsers, softDeleteHandler);
 
 
-// usersRouter.post('/', validatePostUsers, postUsersHandler);
+
 
 
 module.exports = usersRouter;
