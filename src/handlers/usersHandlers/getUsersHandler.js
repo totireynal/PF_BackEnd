d461d23aa28d34c4ef43335b5800dd7d1f23e823
totireynal <sofiareynalm@gmail.com>
2023-03-30 00:47:50 -0300

    arreglo rutas

--- src/handlers/usersHandlers/getUsersHandler.js
+++ src/handlers/usersHandlers/getUsersHandler.js
@@ -3,26 +3,26 @@ const getUsersController = require("../../controllers/usersControllers/getContro
 // const sortUsersController = require('../../controllers/usersControllers/sortControllers/sortUserController');
 const getUsersUnsorted = require("../../controllers/usersControllers/getControllers/getUsersUnsorted.js");
 const getUsersHandler = async (req, res) => {
-  // const { name, role, area, position, sort} = req.query;
+  const { name, role, area, position, sort} = req.query;
   const {CompanyId} = req.params;
-  // console.log(req.params)
-  // console.log(req.query)
+  console.log(req.params)
+  console.log(req.query)
   
   try {
-    // if (CompanyId  && (name || role || area || position || sort)) {
-    //   const resultsUsers = await getUsersController(
-    //     name,
-    //     role,
-    //     area,
-    //     position,
-    //     sort,
-    //     CompanyId
-    //   );
-    //   return res.status(201).json(resultsUsers);
-    // } else {
+    if (CompanyId  && (name || role || area || position || sort)) {
+      const resultsUsers = await getUsersController(
+        name,
+        role,
+        area,
+        position,
+        sort,
+        CompanyId
+      );
+      return res.status(201).json(resultsUsers);
+    } else {
       const resultUsers = await getUsersUnsorted(CompanyId);
       return res.status(200).json(resultUsers);
-    // }
+    }
   } catch (error) {
     return res.status(404).json({ error: error.message });
   }
