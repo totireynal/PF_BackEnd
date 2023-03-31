00850f0186b4482fba6ec94f1bfcda35bf874364
totireynal <sofiareynalm@gmail.com>
2023-03-31 13:15:31 -0300

    arreglo middleware Cuil and Cby

--- src/middlewares/userMiddlewares/validatePostUsersbyCuilAndCbu.js
+++ src/middlewares/userMiddlewares/validatePostUsersbyCuilAndCbu.js
@@ -5,30 +5,18 @@ const Users = require('../../models').Users;
 const validatePostUsersByCuilAndCbu = async (req, res, next) => {
     const { cuil, cbu} = req.body;
     
-    // const findInFile = await File.findAll({
-    //     where: 
-    //      { 
-    //         [Op.or]: [
-    //             {cbu:cbu},
-    //             {cuil:cuil}
-    //         ]
-    //     }
-    // }) 
-     
-
     const findByCbu = await File.findOne({
         where: 
          { cbu: cbu}
     }) 
     const findByCuil = await File.findOne({
         where: 
-         { cuil:cuil}
+         { cuil: cuil}
     }) 
     
-    if (findByCbu ) res.status(400).json({error: `Cuil: ${cuil} already exists in system. Please try another one.`});
-    if (findByCuil) return res.status(400).json({error: `Cuil: ${cbu} already exists in system. Please try another one.`});
-    // if (findInFile) return res.status(400).json({error: `Cuil: ${cuil} already exists in system. Please try another one.`});
-    // if (findInFile) return res.status(400).json({error: `Cbu: ${cbu} already exists in system. Please try another one.`});
+    if (findByCuil ) return res.status(400).json({error: `Cuil: ${cuil} already exists in system. Please try another one.`});
+    if (findByCbu) return res.status(400).json({error: `Cbu: ${cbu} already exists in system. Please try another one.`});
+ 
         
     next();
 }
