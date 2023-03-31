3f0eca08444d6bf260db5bcdaee122ae8a44d548
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-31 16:36:53 -0500

    Validate Users Repeated

--- src/controllers/usersControllers/getControllers/getValidateUserController.js
+++ src/controllers/usersControllers/getControllers/getValidateUserController.js
@@ -1,48 +1,88 @@
-const Users = require('../../models').Users;
-const File = require('../../models').File;
+const Users = require("../../models").Users;
+const File = require("../../models").File;
 
-const getValidateUserController = async (req) => {
-    const { email, dni, tel, cuil, cbu, CompanyId } = req.body;
+const getValidateUserController = async (email, dni, tel, cuil, cbu) => {
+  const { CompanyId } = req.body;
 
+  if (email) {
     const findByEmail = await Users.findOne({
-        where: { 
-            email: email,
-            CompanyId: CompanyId
-        }
+      where: {
+        email: email,
+        CompanyId: CompanyId,
+      },
     });
+    if (findByEmail) {
+      return {
+        message: `Cbu: ${cbu} already exists in system. Please try another one.`,
+      };
+    } else {
+      return null;
+    }
+  }
 
+  if (dni) {
     const findByDni = await Users.findOne({
-        where: { 
-            dni: dni, 
-            CompanyId: CompanyId
-        }
+      where: {
+        dni: dni,
+        CompanyId: CompanyId,
+      },
     });
+    if (findByDni) {
+      return {
+        message: `The dni: ${dni} already exists in system. Please try another one.`,
+      };
+    } else {
+      return null;
+    }
+  }
 
+  if (tel) {
     const findByTel = await Users.findOne({
-        where: { 
-            tel: tel,
-            CompanyId: CompanyId
-        }
+      where: {
+        tel: tel,
+        CompanyId: CompanyId,
+      },
     });
+    if (findByTel) {
+      return {
+        message: `The phone: ${tel} already exists in system. Please try another one.`,
+      };
+    } else {
+      return null;
+    }
+  }
 
+  if (cbu) {
     const findByCbu = await File.findOne({
-        where: { 
-            cbu: cbu,
-            CompanyId: CompanyId
-        }
-    }) 
+      where: {
+        cbu: cbu,
+        CompanyId: CompanyId,
+      },
+    });
+    if (findByCbu) {
+      return {
+        message: `Cbu: ${cbu} already exists in system. Please try another one.`,
+      };
+    } else {
+      return null;
+    }
+  }
+
+  if (cuil) {
     const findByCuil = await File.findOne({
-        where: { 
-            cuil:cuil,
-            CompanyId: CompanyId
-        }
-    }) 
-    
-    if (findByCbu) throw new Error`Cbu: ${cbu} already exists in system. Please try another one.`
-    if (findByCuil ) throw new Error`Cuil: ${cuil} already exists in system. Please try another one.`
-    if(findByEmail) throw new Error`The email: ${email} already exists in system. Please try another one.`
-    if(findByDni) throw new Error`The dni: ${dni} already exists in system. Please try another one.`
-    if(findByTel) throw new Error`The phone: ${tel} already exists in system. Please try another one.`
+      where: {
+        cuil: cuil,
+        CompanyId: CompanyId,
+      },
+    });
+    if (findByCuil) {
+      return {
+        message: `Cuil: ${cuil} already exists in system. Please try another one.`,
+      };
+    } else {
+      return null;
+    }
+  }
 };
 
 module.exports = getValidateUserController;
