28efaa45a549937864955fb0a33de87ed2498b9d
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-31 10:42:23 -0500

    Validacion de datos

--- src/controllers/usersControllers/postController/postUsersController.js
+++ src/controllers/usersControllers/postController/postUsersController.js
@@ -1,40 +1,58 @@
-const Users = require('../../../models').Users;
-const File = require('../../../models').File;
+const Users = require("../../../models").Users;
+const File = require("../../../models").File;
 
-
-
-const postUsersController = async (name, lastName, email, birthDate, address, CompanyId, image, dni, tel, role,  PositionId, AreaId, dateOfAdmission, cuil, cbu) => {
-    let newUser = await Users.create({
-        name,
-        lastName,
-        email,
-        birthDate,
-        address,
-        image,
-        dni,
-        tel,
-        role,
-        CompanyId,
-    })
-
-    // await newUser.setCompany(CompanyId) 
-    const idNewUser = newUser.id;
-    
-    await File.create({
-        dateOfAdmission,
-        cuil,
-        cbu,
-        PositionId ,
-        AreaId,
-        UserId : idNewUser
-    })
-
-    return (`The employee ${name} ${lastName} with id: ${idNewUser} has been created correctly.`)
-    
+const postUsersController = async (
+  name,
+  lastName,
+  email,
+  birthDate,
+  address,
+  CompanyId,
+  image,
+  dni,
+  tel,
+  role,
+  PositionId,
+  AreaId,
+  dateOfAdmission,
+  cuil,
+  cbu
+) => {
+  if ((email, dni, tel)) {
+    const results = await Users.findAll({
+      where: {
+        email: email,
+        dni: dni,
+        tel: tel,
+      },
+    });
+    if (results) throw new Error `The parameters already exist, please try other ones`;
 }
 
+    let newUser = await Users.create({
+      name,
+      lastName,
+      email,
+      birthDate,
+      address,
+      image,
+      dni,
+      tel,
+      role,
+      CompanyId,
+    });
+    const idNewUser = newUser.id;
 
-  
+    await File.create({
+      dateOfAdmission,
+      cuil,
+      cbu,
+      PositionId,
+      AreaId,
+      UserId: idNewUser,
+    });
 
+    return `The employee ${name} ${lastName} with id: ${idNewUser} has been created correctly.`;
+};
 
 module.exports = postUsersController;
