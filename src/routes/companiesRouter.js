d8bee04e26345b86d65511c30b2fed9767aaa2d0
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-04-01 10:24:38 -0500

    Elimino ruta de Companies

--- src/routes/companiesRouter.js
+++ src/routes/companiesRouter.js
@@ -1,7 +1,6 @@
 const companiesRouter = require('express').Router();
 const validatePostCompanies = require('../middlewares/companyMiddlewares/validatePostCompanies');
 const validateCompanyUpdate = require('../middlewares/companyMiddlewares/validatePutCompany');
-const validatePostCompaniesRepeated = require('../middlewares/companyMiddlewares/validatePostCompaniesRepeated');
 const softDeleteCompanyHandler = require('../handlers/companyHandlers/softDeleteCompanyHandler');
 const softDeleteCompany = require('../middlewares/companyMiddlewares/validateSoftDeleteCompany');
 const postCompaniesHandler = require('../handlers/companyHandlers/postCompaniesHandler');
@@ -9,7 +8,7 @@ const putCompanyHandler = require('../handlers/companyHandlers/putCompanyHandler
 const getCompanyValidateHandler = require('../handlers/companyHandlers/getCompanyValidateHandler');
 
 companiesRouter.get('/', getCompanyValidateHandler);
-companiesRouter.post('/register', validatePostCompanies, validatePostCompaniesRepeated, postCompaniesHandler)
+companiesRouter.post('/register', validatePostCompanies, postCompaniesHandler)
 companiesRouter.delete('/:id', softDeleteCompany, softDeleteCompanyHandler);
 companiesRouter.put('/:id', validateCompanyUpdate, putCompanyHandler);
 
