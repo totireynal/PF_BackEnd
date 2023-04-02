d4908af19be5766396c858d78c506df4d4cdf836
totireynal <sofiareynalm@gmail.com>
2023-04-02 15:56:56 -0400

    nueva ruta EmployeeCount

--- src/utils/getCompanyCleanDb.js
+++ src/utils/getCompanyCleanDb.js
@@ -1,16 +1,14 @@
-const getCompanyCleanDb = (array) => {
-    return array.map((element) => {
-        return {
-            id: element.id,
-            name: element.name,
-            cuit: element.cuit,
-            industry: element.industry,
-            location: element.location,
-            numberEmployees: element.numberEmployees,
-            tel: element.tel,
-            email: element.email
+const getCompanyCleanDb = (object) => {
+    return {
+            id: object.id,
+            name: object.name,
+            cuit: object.cuit,
+            industry: object.industry,
+            location: object.location,
+            numberEmployees: object.numberEmployees,
+            tel: object.tel,
+            email: object.email
         }
-    });
-};
+    };
 
 module.exports = getCompanyCleanDb;
