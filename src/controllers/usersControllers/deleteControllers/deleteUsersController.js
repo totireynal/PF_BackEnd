c89b84ef301d4ce73ea5ecbd7d177edbe1181415
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-04-04 09:54:12 -0500

    Eliminados en el año

--- src/controllers/usersControllers/deleteControllers/deleteUsersController.js
+++ src/controllers/usersControllers/deleteControllers/deleteUsersController.js
@@ -1,7 +1,5 @@
 const Users = require('../../../models').Users;
 const File = require('../../../models').File;
-// const Area = require('../../../models').Area;
-// const Position = require('../../../models').Position;
 
 const softDeleteUsers = async (id) => {
     
@@ -16,17 +14,6 @@ const softDeleteUsers = async (id) => {
             })
         }]
     })
-    //  await Users.destroy({
-    //     where: {
-    //         id ,
-    //     },include :[{
-    //         model: File.destroy({
-    //             where: {
-    //                 UserId : id ,
-    //             }}),
-    //      }]
-        
-    // })
     return `The user ${id} has been deleted`;
 }
 
