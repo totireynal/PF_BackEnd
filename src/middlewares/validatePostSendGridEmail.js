683ca599b2723611d11935f9607b0631fbc0f616
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-04-04 16:21:31 -0500

    Email de cumple y middlewares

--- src/middlewares/validatePostSendGridEmail.js
+++ src/middlewares/validatePostSendGridEmail.js
@@ -1,4 +1,4 @@
-const validatePostSendGridAuto = (req, res, next) => {
+const validatePostSendGridEmail = (req, res, next) => {
     const { to } = req.body;
 
     if(!to) return res.status(400).json({ error: 'Missing to' });
@@ -6,4 +6,4 @@ const validatePostSendGridAuto = (req, res, next) => {
     next();
 };
 
-module.exports = validatePostSendGridAuto;
+module.exports = validatePostSendGridEmail;
--- src/routes/sendGridRouter.js
+++ src/routes/sendGridRouter.js
@@ -2,9 +2,11 @@ const sendGrid = require('express').Router();
 const sendGridHandler = require('../handlers/sendGridHandler/sendGridHandler');
 const sendGridHandlerAuto = require('../handlers/sendGridHandler/sendGridHandlerAuto');
 const validatePostSendGrid = require('../middlewares/validatePostSendGrid');
-const validatePostSendGridAuto = require('../middlewares/validatePostSendGridAuto');
+const validatePostSendGridEmail = require('../middlewares/validatePostSendGridEmail');
+const sendGridBrithdayHandler = require('../handlers/sendGridHandler/sendGridBrithdayHandler');
 
 sendGrid.post('/', validatePostSendGrid, sendGridHandler);
-sendGrid.post('/confirmation', validatePostSendGridAuto, sendGridHandlerAuto);
+sendGrid.post('/birthday', validatePostSendGridEmail, sendGridBrithdayHandler);
+sendGrid.post('/confirmation', validatePostSendGridEmail, sendGridHandlerAuto);
 
 module.exports = sendGrid;
