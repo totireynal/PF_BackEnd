424b5d1757332f9f22dbf747f85e5b3358f69cfe
nottotem <totemstu@gmail.com>
2023-04-04 20:02:47 -0300

    commit

--- src/routes/sendgridRouter.js
+++
@@ -1,6 +0,0 @@
-const sendGrid = require("express").Router();
-const sendGridHandler = require("../handlers/sendGridHandler/sendGridHandler");
-
-sendGrid.post("/", sendGridHandler);
-
-module.exports = sendGrid;
