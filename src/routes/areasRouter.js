88e9a5e134f8d6c6f114d82c028ddb40fa5f4bf4
juanavila <juan.avila@live.com.ar>
2023-04-04 11:39:43 -0300

    c

--- src/routes/areasRouter.js
+++ src/routes/areasRouter.js
@@ -4,13 +4,10 @@ const getAllAreasHandler = require('../handlers/areasHandlers/areasCrudHandler.j
 const postAreasHandler = require('../handlers/areasHandlers/areasCrudHandler.js/postAreasHandler');
 const putAreasHandler = require('../handlers/areasHandlers/areasCrudHandler.js/putAreasHandler');
 const getAreasHandler = require('../handlers/areasHandlers/getAreasHandler');
-const getIndexAreaHandler = require('../handlers/areasHandlers/getIndexAreaHandlers');
 
 
-areasRouter.get('/:CompanyId/index', getIndexAreaHandler);
 areasRouter.get('/ars/:CompanyId' ,getAllAreasHandler);
 areasRouter.get('/:CompanyId',  getAreasHandler);
-areasRouter.get('/:CompanyId/index', getIndexAreaHandler)
 areasRouter.post('/',postAreasHandler);
 areasRouter.put('/:id',putAreasHandler);
 areasRouter.delete('/:id',deleteAreasHandler);
