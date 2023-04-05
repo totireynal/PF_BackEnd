20c312588e6e2041b3d11ef5589ea40dbb55c470
totireynal <sofiareynalm@gmail.com>
2023-04-05 04:19:06 -0400

    cambio seeders final

--- src/seeders/20230316023047-Companies.js
+++ src/seeders/20230316023047-Companies.js
@@ -12,7 +12,7 @@ module.exports = {
         
         // id: uuidv4(),
         name: 'CocaCola',
-        cuit: '123456789',
+        cuit: '20123422888',
         industry: 'Beverages',
         location: 'Buenos Aires',
         numberEmployees: 10,
@@ -42,37 +42,6 @@ module.exports = {
       
 
       },
-      {
-        
-        // id: uuidv4(),
-        name: 'Arcor',
-        cuit: '123456888',
-        industry: 'Food and Beverages',
-        location: 'Cordoba',
-        numberEmployees: 30,
-        image: 'https://i.pinimg.com/736x/f4/7e/29/f47e29ea9caa12afdfe0b9f4ab8d7c7a.jpg',
-        tel: '990987125',
-        email: 'arcor@gmail.com',
-        authorized: true,
-        paymentDay:'12-02-2023',
-        InformationId: 1,
-      
-
-      },
-      {
-        name: 'Pepsi',
-        cuit: '87654321',
-        industry: 'Bebidas',
-        location: 'Buenos Aires',
-        numberEmployees: 10,
-        image: 'https://graffica.info/wp-content/uploads/2023/02/cocacola-logo.jpeg',
-        tel: '47495862',
-        email: 'empresa2@gmail.com',
-        authorized: true,
-        paymentDay:'12-03-94',
-        InformationId: 1,
-      
-      }
     ])
     /**
      * Add seed commands here.
